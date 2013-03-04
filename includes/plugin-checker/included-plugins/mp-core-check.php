78781cdd8d60728870b14ab462373eacda78e1f2
Phil Johnston <johnstonphilip@gmail.com>
2013-03-03 22:38:02 -0500

    mp-core check
    Signed-off-by: Phil Johnston <johnstonphilip@gmail.com>

--- includes/plugin-checker/included-plugins/mp-core-check.php
+++ includes/plugin-checker/included-plugins/mp-core-check.php
@@ -8,11 +8,11 @@
 		$args = array(
 			'plugin_name' => __('Move Plugins Core', 'mp_stacks'), 
 			'plugin_message' => __('You require the Move Plugins Core plugin. Install it here.', 'mp_stacks'), 
-			'plugin_slug' => 'mp_core', 
+			'plugin_slug' => 'mp-core', 
 			'plugin_subdirectory' => 'mp-core/', 
 			'plugin_filename' => 'mp-core.php',
 			'plugin_required' => true,
-			'plugin_download_link' => 'http://moveplugins.com'
+			'plugin_download_link' => 'http://moveplugins.com/download.php?download=mp-core'
 		);
 		$mp_core_plugin_check = new MP_CORE_Plugin_Checker($args);
 	}
