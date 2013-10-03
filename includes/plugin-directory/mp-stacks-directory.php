a2ce482a3ea9eb777174cfa26ef2792c38bcdb92
Phil Johnston <johnstonphilip@gmail.com>
2013-10-03 18:59:05 -0400

    beta1.0.2.0
    Signed-off-by: Phil Johnston <johnstonphilip@gmail.com>

--- includes/plugin-directory/mp-stacks-directory.php
+++ includes/plugin-directory/mp-stacks-directory.php
@@ -9,7 +9,7 @@ function mp_stacks_plugin_directory(){
 		'parent_slug' => 'edit.php?post_type=mp_brick',
 		'page_title' => 'Add Ons',
 		'slug' => 'mp_stacks_plugin_directory',
-		'directory_list_url' => 'http://repo.moveplugins.com/repo-group/mp-stacks/'
+		'directory_list_url' => 'http://moveplugins.com/repo-group/mp-stacks/'
 	);
 	
 	new MP_CORE_Plugin_Directory( $args );
