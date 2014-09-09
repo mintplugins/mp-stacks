2e60aa0be06e83be5bca729ef4b381252b85c0bf
Phil Johnston <johnstonphilip@gmail.com>
2014-09-09 16:49:22 -0400

    Tip and Support Message above Brick Editor
    Added a dismissable tip about double clicking and a notice to email for support if needed - above the brick editor.
    
    Signed-off-by: Phil Johnston <johnstonphilip@gmail.com>

---
+++ includes/metaboxes/mp-stacks-support/mp-stacks-support.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Function which create the admin notice for the mp_brick editor
+ *
+ */
+function mp_stacks_support_admin_notice(){
+	 
+	 //Only load message if mp_stack_id is set
+	 if ( !isset( $_GET['mp_stack_id'] ) ){
+		return; 
+	 }
+	 
+	 $stack_info = get_term( $_GET['mp_stack_id'], 'mp_stacks' );
+	 
+	 ?>
+	 <div class="updated">
+        <p><?php echo '<img src="' . plugins_url( 'assets/images/mp_stack-icon-2x.png', dirname(__FILE__) ) . '" />' . __( 'You are editing a "Brick" in the "Stack" called "' . $stack_info->name . '".', 'mp_stacks'); ?>
+		<em><?php echo __(' Having trouble? Feel free to email us: support@mintplugins.com. Estimated response time is 12-24 hours.', 'mp_stacks' ); ?></em></p>
+     </div>
+     <?php
+	
+	
+	
+}
+add_action('admin_notices', 'mp_stacks_support_admin_notice');
+
+/**
+ * Function which create the admin notice for the mp_brick editor
+ *
+ */
+function mp_stacks_double_click_tip(){
+	 
+	 //Only load message if mp_stack_id is set
+	 if ( !isset( $_GET['mp_stack_id'] ) ){
+		return; 
+	 }
+	 
+	 $stack_info = get_term( $_GET['mp_stack_id'], 'mp_stacks' );
+	 
+	 ?>
+	 <div class="updated">
+        <p><?php echo __( 'TIP: You can open this Brick editor at any time by double clicking <em>anywhere</em> on a brick.', 'mp_stacks'); ?>
+     </div>
+     <?php
+	
+	
+	
+}
+add_action('admin_notices', 'mp_stacks_double_click_tip');
