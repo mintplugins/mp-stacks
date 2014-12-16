37fa083c634a628ead51cc71f63ddf605a547c8d
Phil Johnston <johnstonphilip@gmail.com>
2014-12-16 15:10:37 -0500

    Changed Brick Post Type Priority
    Set Priority to 1 and removed needless if statement from previous release

--- includes/custom-post-types/bricks.php
+++ includes/custom-post-types/bricks.php
@@ -24,42 +24,41 @@
  * @return   void
  */
 function mp_brick_post_type() {
+
+	$brick_labels =  apply_filters( 'mp_stacks_brick_labels', array(
+		'name' 				=> 'Bricks',
+		'singular_name' 	=> 'Brick Item',
+		'add_new' 			=> __('Add New Brick', 'mp_stacks'),
+		'add_new_item' 		=> __('Add New Brick', 'mp_stacks'),
+		'edit_item' 		=> __('Edit Brick', 'mp_stacks'),
+		'new_item' 			=> __('New Brick', 'mp_stacks'),
+		'all_items' 		=> __('Manage Bricks', 'mp_stacks'),
+		'view_item' 		=> __('View Bricks', 'mp_stacks'),
+		'search_items' 		=> __('Search Bricks', 'mp_stacks'),
+		'not_found' 		=>  __('No Bricks found', 'mp_stacks'),
+		'not_found_in_trash'=> __('No Bricks found in Trash', 'mp_stacks'), 
+		'parent_item_colon' => '',
+		'menu_name' 		=> __('Stacks & Bricks', 'mp_stacks')
+	) );
 	
-	if (mp_core_get_option( 'mp_stacks_settings_general',  'enable_disable' ) != 'disabled' ){
-		$brick_labels =  apply_filters( 'mp_stacks_brick_labels', array(
-			'name' 				=> 'Bricks',
-			'singular_name' 	=> 'Brick Item',
-			'add_new' 			=> __('Add New Brick', 'mp_stacks'),
-			'add_new_item' 		=> __('Add New Brick', 'mp_stacks'),
-			'edit_item' 		=> __('Edit Brick', 'mp_stacks'),
-			'new_item' 			=> __('New Brick', 'mp_stacks'),
-			'all_items' 		=> __('Manage Bricks', 'mp_stacks'),
-			'view_item' 		=> __('View Bricks', 'mp_stacks'),
-			'search_items' 		=> __('Search Bricks', 'mp_stacks'),
-			'not_found' 		=>  __('No Bricks found', 'mp_stacks'),
-			'not_found_in_trash'=> __('No Bricks found in Trash', 'mp_stacks'), 
-			'parent_item_colon' => '',
-			'menu_name' 		=> __('Stacks & Bricks', 'mp_stacks')
-		) );
 		
-			
-		$brick_args = array(
-			'labels' 			=> $brick_labels,
-			'public' 			=> false,
-			'publicly_queryable'=> false,
-			'show_ui' 			=> true, 
-			'show_in_menu' 		=> false, 
-			'query_var' 		=> true,
-			'rewrite' 			=> array( 'slug' => 'stack' ),
-			'capability_type' 	=> 'page',
-			'has_archive' 		=> false, 
-			'hierarchical' 		=> true,
-			'supports' 			=> apply_filters('mp_stacks_brick_supports', array( 'title') ),
-		); 
-		register_post_type( 'mp_brick', apply_filters( 'mp_stacks_brick_post_type_args', $brick_args ) );
-	}
+	$brick_args = array(
+		'labels' 			=> $brick_labels,
+		'public' 			=> false,
+		'publicly_queryable'=> false,
+		'show_ui' 			=> true, 
+		'show_in_menu' 		=> false, 
+		'query_var' 		=> true,
+		'rewrite' 			=> array( 'slug' => 'stack' ),
+		'capability_type' 	=> 'page',
+		'has_archive' 		=> false, 
+		'hierarchical' 		=> true,
+		'supports' 			=> apply_filters('mp_stacks_brick_supports', array( 'title') ),
+	); 
+	register_post_type( 'mp_brick', apply_filters( 'mp_stacks_brick_post_type_args', $brick_args ) );
+
 }
-add_action( 'init', 'mp_brick_post_type', 0 );
+add_action( 'init', 'mp_brick_post_type', 1 );
 
 /**
  * MP Stacks Custom Taxonomy
@@ -72,38 +71,36 @@ add_action( 'init', 'mp_brick_post_type', 0 );
  * @return   void
  */
 function mp_stacks_taxonomy() {  
-	if (mp_core_get_option( 'mp_stacks_settings_general',  'enable_disable' ) != 'disabled' ){
 		
-		// Add new taxonomy, make it hierarchical (like categories)
-		$labels = array(
-			'name'                => __( 'Manage Stacks', 'mp_core' ),
-			'singular_name'       => __( 'Stack', 'mp_core' ),
-			'search_items'        => __( 'Search Stacks', 'mp_core' ),
-			'all_items'           => __( 'All Stacks', 'mp_core' ),
-			'parent_item'         => __( 'Parent Stack', 'mp_core' ),
-			'parent_item_colon'   => __( 'Parent Stack:', 'mp_core' ),
-			'edit_item'           => __( 'Edit Stack', 'mp_core' ), 
-			'update_item'         => __( 'Update Stack', 'mp_core' ),
-			'add_new_item'        => __( 'Add New Stack', 'mp_core' ),
-			'new_item_name'       => __( 'New Stack Name', 'mp_core' ),
-			'menu_name'           => __( 'Manage Stacks', 'mp_core' ),
-		); 	
-  
-		register_taxonomy(  
-			'mp_stacks',  
-			'mp_brick',  
-			array(  
-				'hierarchical' => true,  
-				'label' => 'Stacks',  
-				'labels' => $labels,  
-				'query_var' => true,  
-				'with_front' => false, 
-				'rewrite' => array('slug' => 'stacks')  
-			)  
-		);  
-	}
+	// Add new taxonomy, make it hierarchical (like categories)
+	$labels = array(
+		'name'                => __( 'Manage Stacks', 'mp_core' ),
+		'singular_name'       => __( 'Stack', 'mp_core' ),
+		'search_items'        => __( 'Search Stacks', 'mp_core' ),
+		'all_items'           => __( 'All Stacks', 'mp_core' ),
+		'parent_item'         => __( 'Parent Stack', 'mp_core' ),
+		'parent_item_colon'   => __( 'Parent Stack:', 'mp_core' ),
+		'edit_item'           => __( 'Edit Stack', 'mp_core' ), 
+		'update_item'         => __( 'Update Stack', 'mp_core' ),
+		'add_new_item'        => __( 'Add New Stack', 'mp_core' ),
+		'new_item_name'       => __( 'New Stack Name', 'mp_core' ),
+		'menu_name'           => __( 'Manage Stacks', 'mp_core' ),
+	); 	
+
+	register_taxonomy(  
+		'mp_stacks',  
+		'mp_brick',  
+		array(  
+			'hierarchical' => true,  
+			'label' => 'Stacks',  
+			'labels' => $labels,  
+			'query_var' => true,  
+			'with_front' => false, 
+			'rewrite' => array('slug' => 'stacks')  
+		)  
+	);  
 }  
-add_action( 'init', 'mp_stacks_taxonomy' ); 
+add_action( 'after_setup_theme', 'mp_stacks_taxonomy', 1 ); 
 
 /**
  * Change the titles of the columns on the "Manage Stacks" page
