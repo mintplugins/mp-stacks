94a39762b2e803d85110f2f97698f59615757f52
Phil Johnston <johnstonphilip@gmail.com>
2014-12-19 19:25:52 -0500

    Added array keys to the text meta options

--- includes/metaboxes/mp-stacks-text/mp-stacks-singletext.php
+++ includes/metaboxes/mp-stacks-text/mp-stacks-singletext.php
@@ -21,7 +21,7 @@ function mp_stacks_singletext_create_meta_box(){
 	 *
 	 */
 	$mp_stacks_singletext_items_array = array(
-		array(
+		'brick_text_title' => array(
 			'field_id'	 => 'brick_text_title',
 			'field_title' => __( 'Text Area', 'mp_stacks'),
 			'field_description' => __( 'Add as many Text Areas as you want. Each new text area can have its own font, text-size, color, and spacing.', 'mp_stacks' ),
@@ -29,43 +29,43 @@ function mp_stacks_singletext_create_meta_box(){
 			'field_value' => '',
 			'field_repeater' => 'mp_stacks_singletext_content_type_repeater'
 		),
-		array(
+		'brick_text_color' => array(
 			'field_id'	 => 'brick_text_color',
 			'field_title' => __( 'Text Color', 'mp_stacks'),
-			'field_description' => '<br />Select the color of text area 1.',
+			'field_description' => 'Select the color for this text.',
 			'field_type' => 'colorpicker',
 			'field_value' => '',
 			'field_container_class' => 'mp_brick_text_option',
 			'field_repeater' => 'mp_stacks_singletext_content_type_repeater'
 		),
-		array(
+		'brick_text_font_size' => array(
 			'field_id'	 => 'brick_text_font_size',
 			'field_title' => __( 'Text Size', 'mp_stacks'),
-			'field_description' => '<br />Enter the size (in pixels).',
+			'field_description' => 'Enter the size (in pixels).',
 			'field_type' => 'number',
 			'field_value' => '35',
 			'field_container_class' => 'mp_brick_text_option',
 			'field_repeater' => 'mp_stacks_singletext_content_type_repeater'
 		),
-		array(
+		'brick_text_line_height' => array(
 			'field_id'	 => 'brick_text_line_height',
 			'field_title' => __( 'Line-Height', 'mp_stacks'),
-			'field_description' => '<br />Enter the line-height in pixels. By default this matches the Text Size.',
+			'field_description' => 'Enter the line-height in pixels. By default this matches the Text Size.',
 			'field_type' => 'number',
 			'field_value' => '',
 			'field_container_class' => 'mp_brick_text_option',
 			'field_repeater' => 'mp_stacks_singletext_content_type_repeater'
 		),
-		array(
+		'brick_text_paragraph_margin_bottom' => array(
 			'field_id'	 => 'brick_text_paragraph_margin_bottom',
 			'field_title' => __( 'Paragraph Spacing', 'mp_stacks'),
-			'field_description' => '<br />Enter the number of pixels separating each paragraph. Default: 15px',
+			'field_description' => 'Enter the number of pixels separating each paragraph in this text area. Default: 15px',
 			'field_type' => 'number',
 			'field_value' => '15',
 			'field_container_class' => 'mp_brick_text_option',
 			'field_repeater' => 'mp_stacks_singletext_content_type_repeater'
 		),
-		array(
+		'brick_text' => array(
 			'field_id'	 => 'brick_text',
 			'field_title' => __( 'Text Area', 'mp_stacks'),
 			'field_description' => 'Enter the text to display in this text area',
