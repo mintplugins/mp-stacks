cd50e617eac0e49577c618b19b0013c048d21fa9
Phil Johnston <johnstonphilip@gmail.com>
2015-01-25 17:45:52 -0500

    Main Image Lightbox Added
    The main image can now open in a lightbox by default without the MIL addon - this addon will be phased out

--- includes/metaboxes/mp-stacks-image/mp-stacks-image.php
+++ includes/metaboxes/mp-stacks-image/mp-stacks-image.php
@@ -21,28 +21,65 @@ function mp_stacks_image_create_meta_box(){
 	 *
 	 */
 	$mp_stacks_image_items_array = array(
-		array(
+		'brick_main_image' => array(
 			'field_id'			=> 'brick_main_image',
 			'field_title' 	=> __( 'Main Image', 'mp_stacks'),
 			'field_description' 	=> 'Select an image to display beside the text on this stack item.',
 			'field_type' 	=> 'mediaupload',
 			'field_value' => ''
 		),
-		array(
-			'field_id'			=> 'brick_main_image_max_width',
-			'field_title' 	=> __( 'Max Image Width (Optional)', 'mp_stacks'),
-			'field_description' 	=> 'Set the maximum width (in pixels) this image should show at. Image will shrink to fit smaller screens but never get bigger than this width.',
-			'field_type' 	=> 'number',
-			'field_value' => ''
+		'brick_main_image_max_width' => array(
+				'field_id'			=> 'brick_main_image_max_width',
+				'field_title' 	=> __( 'Max Image Width (Optional)', 'mp_stacks'),
+				'field_description' 	=> 'Set the maximum width (in pixels) this image should show at. Image will shrink to fit smaller screens but never get bigger than this width.',
+				'field_type' 	=> 'number',
+				'field_value' => '',
+			),
+		'brick_main_image_link_showhider' => array( 
+			'field_id'			=> 'brick_main_image_link_showhider',
+			'field_title' 	=> __( 'Image Link Settings', 'mp_stacks'),
+			'field_description' 	=> '',
+			'field_type' 	=> 'showhider',
+			'field_value' => '',
 		),
-		array(
-			'field_id'			=> 'brick_main_image_link_url',
-			'field_title' 	=> __( 'Link URL', 'mp_stacks'),
-			'field_description' 	=> 'Enter the URL the above image will go to when clicked. EG: http://mylink.com',
-			'field_type' 	=> 'url',
-			'field_value' => ''
-		),
-		array(
+			'brick_main_image_link_url' => array(
+				'field_id'			=> 'brick_main_image_link_url',
+				'field_title' 	=> __( 'Link URL', 'mp_stacks'),
+				'field_description' 	=> 'Enter the URL the above image will go to when clicked. EG: http://mylink.com',
+				'field_type' 	=> 'url',
+				'field_value' => '',
+				'field_showhider' => 'brick_main_image_link_showhider',
+			),
+			'brick_main_image_open_type' => array( 
+				'field_id'			=> 'brick_main_image_open_type',
+				'field_title' 	=> __( 'Link Open Type', 'mp_stacks'),
+				'field_description' 	=> 'Enter the URL the above image will go to when clicked. EG: http://mylink.com',
+				'field_type' 	=> 'select',
+				'field_value' => '',
+				'field_select_values' => array( 'lightbox' => __( 'Open in Lightbox', 'mp_stacks' ), 'parent' => __( 'Open in current Window/Tab', 'mp_stacks' ), 'blank' => __( 'Open in New Window/Tab', 'mp_stacks' ) ),
+				'field_showhider' => 'brick_main_image_link_showhider',
+			),
+			'brick_main_image_lightbox_width' => array( 
+				'field_id'			=> 'brick_main_image_lightbox_width',
+				'field_title' 	=> __( 'Lightbox Width', 'mp_stacks'),
+				'field_description' 	=> 'Enter width the lightbox popup should be. Leave blank for 100% of screen.',
+				'field_type' 	=> 'number',
+				'field_value' => '',
+				'field_conditional_id' => 'brick_main_image_open_type',
+				'field_conditional_values' => array( 'lightbox' ),
+				'field_showhider' => 'brick_main_image_link_showhider',
+			),
+			'brick_main_image_lightbox_height' => array( 
+				'field_id'			=> 'brick_main_image_lightbox_height',
+				'field_title' 	=> __( 'Lightbox Height', 'mp_stacks'),
+				'field_description' 	=> 'Enter height the lightbox popup should be. Leave blank for 100% of screen.',
+				'field_type' 	=> 'number',
+				'field_value' => '',
+				'field_conditional_id' => 'brick_main_image_open_type',
+				'field_conditional_values' => array( 'lightbox' ),
+				'field_showhider' => 'brick_main_image_link_showhider',
+			),
+		'brick_content_type_help' => array(
 			'field_id'	 => 'brick_content_type_help',
 			'field_title' => 'Content Types',
 			'field_description' => NULL,
