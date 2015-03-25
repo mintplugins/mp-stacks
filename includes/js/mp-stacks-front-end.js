9d67600621fa0022ce8ca476f5d062edab421f78
Phil Johnston <johnstonphilip@gmail.com>
2015-03-24 22:36:42 -0400

    Brick Auto Scroll is now 100ms instead of 500ms

--- includes/js/mp-stacks-front-end.js
+++ includes/js/mp-stacks-front-end.js
@@ -373,13 +373,16 @@ jQuery(document).ready(function($){
 	 */
 	$(function() {
 	  $('a[href*=#]:not([href=#])').click(function() {
+		var href = $.attr(this, 'href');
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 		  var target = $(this.hash);
 		  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 		  if (target.length) {
 			$('html,body').animate({
 			  scrollTop: target.offset().top
-			}, 1000);
+			}, 500, function(){
+				 window.location.hash = href;	
+			});
 			return false;
 		  }
 		}
