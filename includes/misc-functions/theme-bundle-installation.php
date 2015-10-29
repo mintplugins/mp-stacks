819f07817869d4456e0c49ec7f2a5b95e4100240
Phil Johnston <johnstonphilip@gmail.com>
2015-10-29 18:27:40 -0400

    Make default content setup for Theme Bundles happen via ajax to prevent as many page refreshes - which can be confusing for the user.

--- includes/misc-functions/theme-bundle-installation.php
+++ includes/misc-functions/theme-bundle-installation.php
@@ -100,7 +100,29 @@ function mp_stacks_theme_bundle_custom_installation_settings(){
 			
 		//Additional Custom settings for plugins go here:
 		do_action( 'mp_stacks_additional_installation_actions', $theme_bundle_slug );
+			
+		$mp_core_options['mp_theme_bundle_default_items_setup_success'] = true;
+		
+	}
+}
+add_action( 'admin_init', 'mp_stacks_theme_bundle_custom_installation_settings' );
+
+/**
+ * Default items setup notice. This shows the user a "Success" message if all items have just been successfully set up.
+ *
+ * @since 1.0
+ * @param void
+ * @return void
+ */
+function mp_stacks_theme_bundle_default_items_notice(){
 	
+	global $mp_core_options;
+	
+	//If all default items for this theme bundle have been setup successfully, show the admin notice about the success!	
+	if ( isset( $mp_core_options['mp_theme_bundle_default_items_setup_success'] ) && $mp_core_options['mp_theme_bundle_default_items_setup_success'] ){
+		
+		$theme_bundle_slug = $theme_bundle_slug = $mp_core_options['mp_stacks_theme_bundle_being_installed']['theme_bundle_slug'];
+		
 		?>
 							
 		<div class="welcome-panel">
@@ -126,19 +148,18 @@ function mp_stacks_theme_bundle_custom_installation_settings(){
 		<div id="dashboard-widgets-wrap">
 	
 		</div><!-- dashboard-widgets-wrap -->
-	
-		<?php
+		<?php	
 		
 		//Now that all settings are complete, we can officially stop the installation process by un-setting these:
 		unset( $mp_core_options['parent_plugin_activation_status'] );
 		unset( $mp_core_options['setup_default_' . $theme_bundle_slug. '_items'] );
 		unset( $mp_core_options['setting_default_' . $theme_bundle_slug . '_items'] );	
+		unset( $mp_core_options['mp_theme_bundle_default_items_setup_success'] );
 		unset( $mp_core_options['mp_stacks_theme_bundle_being_installed'] );
 		update_option( 'mp_core_options', $mp_core_options );	
-		
 	}
 }
-add_action( 'admin_notices', 'mp_stacks_theme_bundle_custom_installation_settings' );
+add_action( 'admin_notices', 'mp_stacks_theme_bundle_default_items_notice' );
 
 /**
  * Theme Bundle Installation Function: This function will check if we've created this Theme Bundle's Default Stacks and corresponding Pages/Posts
@@ -305,16 +326,175 @@ function mp_stacks_theme_bundle_create_default_pages( $theme_bundle_slug ){
 			//We do this with JS instead of with wp_redirect because it could give a "Too Many redirects" error.
 			if ( $significant_change_made ){
 				
+				//If we are doing ajax right now, return false here so we dont run double ajax
+				if ( defined( 'DOING_AJAX' ) && DOING_AJAX ){
+					 return false;	
+				}
+				
 				 ?>
                 <div class="updated" style="margin: 19px 0px 19px 0px;">
                     <p><div class="spinner is-active" style="float:left; margin-top: 11px;"></div><h1><?php echo __( 'Please wait', 'mp_stacks' ); ?></h1> <?php echo '<strong>' . $mp_core_options['mp_stacks_theme_bundle_being_installed']['fancy_title'] . '</strong> ' . __( 'is setting up Default Content. This may take a couple of minutes. Thanks for your patience.', 'mp_stacks' ); ?></p>
                 </div>
-                <?php
-	
-				echo '<script type="text/javascript">';
-					echo "window.location = '" . admin_url() . "';";
-				echo '</script>';
+                
+                <script type="text/javascript" src="<?php echo get_bloginfo( 'wpurl' ); ?>/wp-includes/js/jquery/jquery.js"></script>
+				<script type="text/javascript" src="<?php echo MP_CORE_PLUGIN_URL; ?>includes/js/utility/velocity.min.js"></script>
+                    
+				<script type="text/javascript">
+					
+					jQuery(document).ready(function($){
+						
+						$(".small, .small-shadow").velocity({
+							rotateZ: [0,-360]},{
+							loop:true,
+							duration: 2000
+						});
+						$(".medium, .medium-shadow").velocity({
+							rotateZ: -240},{
+							loop:true,
+							duration: 2000
+						});
+						$(".large, .large-shadow").velocity({
+							rotateZ: 180},{
+							loop:true,
+							duration: 2000
+						});
+						
+						function mp_stacks_set_up_theme_bundle_default_content_ajax(){
+							
+							var postData = {
+								action: 'mp_stacks_set_up_theme_bundle_default_content',
+								mp_theme_bundle_slug: '<?php echo $theme_bundle_slug; ?>'
+							};
+							
+							//Run the Ajax
+							$.ajax({
+								type: "POST",
+								data: postData,
+								dataType:"json",
+								url: '<?php echo admin_url( 'admin-ajax.php' ); ?>',
+								success: function (response) {
+									
+									//If all items have been set up
+									if ( response.setup_complete ){
+										//redirect the user to their dashboard
+										window.location = '<?php echo admin_url(); ?>?redirected=true';
+									}
+									//If we still need to set up more things
+									else{
+										//Re-run this function
+										mp_stacks_set_up_theme_bundle_default_content_ajax();
+										
+										console.log( 're-running ajax setup function' );
+									}
+																					
+								}
+							}).fail(function (data) {
+								console.log(data);
+							});
+							
+						}
+						
+						//Start running the ajax setups
+						mp_stacks_set_up_theme_bundle_default_content_ajax();
+						
+					});
+					
+					
+				</script>
 				
+				<style type="text/css">
+					body, html {
+					  width: 100%;
+					  height: 100%;
+					  background-color:#222222;
+					  text-align:center;
+					  color:#fff;
+					  font-family:Verdana, Geneva, sans-serif;
+					}
+					
+					.container {
+					  height: 100%;
+					  display: -webkit-box;
+					  display: -webkit-flex;
+					  display: -ms-flexbox;
+					  display: flex;
+					  -webkit-box-pack: center;
+					  -webkit-justify-content: center;
+						  -ms-flex-pack: center;
+							  justify-content: center;
+					  -webkit-box-align: center;
+					  -webkit-align-items: center;
+						  -ms-flex-align: center;
+							  align-items: center; }
+					
+					.machine {
+					  width: 60vmin;
+					  fill: #fff; }
+					
+					.small-shadow, .medium-shadow, .large-shadow {
+					  fill: rgba(0, 0, 0, 0.05); }
+					
+					.small {
+					  -webkit-transform-origin: 100.136px 225.345px;
+						  -ms-transform-origin: 100.136px 225.345px;
+							  transform-origin: 100.136px 225.345px; }
+					
+					.small-shadow {
+					  -webkit-transform-origin: 110.136px 235.345px;
+						  -ms-transform-origin: 110.136px 235.345px;
+							  transform-origin: 110.136px 235.345px; }
+					
+					.medium {
+					  -webkit-transform-origin: 254.675px 379.447px;
+						  -ms-transform-origin: 254.675px 379.447px;
+							  transform-origin: 254.675px 379.447px; }
+					
+					.medium-shadow {
+					  -webkit-transform-origin: 264.675px 389.447px;
+						  -ms-transform-origin: 264.675px 389.447px;
+							  transform-origin: 264.675px 389.447px; }
+					
+					.large {
+					  -webkit-transform-origin: 461.37px 173.694px;
+						  -ms-transform-origin: 461.37px 173.694px;
+							  transform-origin: 461.37px 173.694px; }
+					
+					.large-shadow {
+					  -webkit-transform-origin: 471.37px 183.694px;
+						  -ms-transform-origin: 471.37px 183.694px;
+							  transform-origin: 471.37px 183.694px; }
+				</style>
+                
+                <div id="notify_user_of_happenings">
+						<div class="container" style="width:200px; margin: 0px auto;">
+							<svg class="machine"xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 645 526">
+							  <defs/>
+							  <g>
+								<path  x="-173,694" y="-173,694" class="large-shadow" d="M645 194v-21l-29-4c-1-10-3-19-6-28l25-14 -8-19 -28 7c-5-8-10-16-16-24L602 68l-15-15 -23 17c-7-6-15-11-24-16l7-28 -19-8 -14 25c-9-3-18-5-28-6L482 10h-21l-4 29c-10 1-19 3-28 6l-14-25 -19 8 7 28c-8 5-16 10-24 16l-23-17L341 68l17 23c-6 7-11 15-16 24l-28-7 -8 19 25 14c-3 9-5 18-6 28l-29 4v21l29 4c1 10 3 19 6 28l-25 14 8 19 28-7c5 8 10 16 16 24l-17 23 15 15 23-17c7 6 15 11 24 16l-7 28 19 8 14-25c9 3 18 5 28 6l4 29h21l4-29c10-1 19-3 28-6l14 25 19-8 -7-28c8-5 16-10 24-16l23 17 15-15 -17-23c6-7 11-15 16-24l28 7 8-19 -25-14c3-9 5-18 6-28L645 194zM471 294c-61 0-110-49-110-110S411 74 471 74s110 49 110 110S532 294 471 294z"/>
+							  </g>
+							  <g>
+								<path x="-136,996" y="-136,996" class="medium-shadow" d="M402 400v-21l-28-4c-1-10-4-19-7-28l23-17 -11-18L352 323c-6-8-13-14-20-20l11-26 -18-11 -17 23c-9-4-18-6-28-7l-4-28h-21l-4 28c-10 1-19 4-28 7l-17-23 -18 11 11 26c-8 6-14 13-20 20l-26-11 -11 18 23 17c-4 9-6 18-7 28l-28 4v21l28 4c1 10 4 19 7 28l-23 17 11 18 26-11c6 8 13 14 20 20l-11 26 18 11 17-23c9 4 18 6 28 7l4 28h21l4-28c10-1 19-4 28-7l17 23 18-11 -11-26c8-6 14-13 20-20l26 11 11-18 -23-17c4-9 6-18 7-28L402 400zM265 463c-41 0-74-33-74-74 0-41 33-74 74-74 41 0 74 33 74 74C338 430 305 463 265 463z"/>
+							  </g>
+							  <g >
+								<path x="-100,136" y="-100,136" class="small-shadow" d="M210 246v-21l-29-4c-2-10-6-18-11-26l18-23 -15-15 -23 18c-8-5-17-9-26-11l-4-29H100l-4 29c-10 2-18 6-26 11l-23-18 -15 15 18 23c-5 8-9 17-11 26L10 225v21l29 4c2 10 6 18 11 26l-18 23 15 15 23-18c8 5 17 9 26 11l4 29h21l4-29c10-2 18-6 26-11l23 18 15-15 -18-23c5-8 9-17 11-26L210 246zM110 272c-20 0-37-17-37-37s17-37 37-37c20 0 37 17 37 37S131 272 110 272z"/>
+							  </g>
+							  <g>
+								<path x="-100,136" y="-100,136" class="small" d="M200 236v-21l-29-4c-2-10-6-18-11-26l18-23 -15-15 -23 18c-8-5-17-9-26-11l-4-29H90l-4 29c-10 2-18 6-26 11l-23-18 -15 15 18 23c-5 8-9 17-11 26L0 215v21l29 4c2 10 6 18 11 26l-18 23 15 15 23-18c8 5 17 9 26 11l4 29h21l4-29c10-2 18-6 26-11l23 18 15-15 -18-23c5-8 9-17 11-26L200 236zM100 262c-20 0-37-17-37-37s17-37 37-37c20 0 37 17 37 37S121 262 100 262z"/>
+							  </g>
+							  <g>
+								<path x="-173,694" y="-173,694" class="large" d="M635 184v-21l-29-4c-1-10-3-19-6-28l25-14 -8-19 -28 7c-5-8-10-16-16-24L592 58l-15-15 -23 17c-7-6-15-11-24-16l7-28 -19-8 -14 25c-9-3-18-5-28-6L472 0h-21l-4 29c-10 1-19 3-28 6L405 9l-19 8 7 28c-8 5-16 10-24 16l-23-17L331 58l17 23c-6 7-11 15-16 24l-28-7 -8 19 25 14c-3 9-5 18-6 28l-29 4v21l29 4c1 10 3 19 6 28l-25 14 8 19 28-7c5 8 10 16 16 24l-17 23 15 15 23-17c7 6 15 11 24 16l-7 28 19 8 14-25c9 3 18 5 28 6l4 29h21l4-29c10-1 19-3 28-6l14 25 19-8 -7-28c8-5 16-10 24-16l23 17 15-15 -17-23c6-7 11-15 16-24l28 7 8-19 -25-14c3-9 5-18 6-28L635 184zM461 284c-61 0-110-49-110-110S401 64 461 64s110 49 110 110S522 284 461 284z"/>
+							  </g>
+							  <g>
+								<path x="-136,996" y="-136,996" class="medium" d="M392 390v-21l-28-4c-1-10-4-19-7-28l23-17 -11-18L342 313c-6-8-13-14-20-20l11-26 -18-11 -17 23c-9-4-18-6-28-7l-4-28h-21l-4 28c-10 1-19 4-28 7l-17-23 -18 11 11 26c-8 6-14 13-20 20l-26-11 -11 18 23 17c-4 9-6 18-7 28l-28 4v21l28 4c1 10 4 19 7 28l-23 17 11 18 26-11c6 8 13 14 20 20l-11 26 18 11 17-23c9 4 18 6 28 7l4 28h21l4-28c10-1 19-4 28-7l17 23 18-11 -11-26c8-6 14-13 20-20l26 11 11-18 -23-17c4-9 6-18 7-28L392 390zM255 453c-41 0-74-33-74-74 0-41 33-74 74-74 41 0 74 33 74 74C328 420 295 453 255 453z"/>
+							  </g>
+							</svg>
+						</div>
+					</div>
+				
+                <?php
+				
+				die();
+                
 				return false;
 			}
 				
@@ -324,3 +504,42 @@ function mp_stacks_theme_bundle_create_default_pages( $theme_bundle_slug ){
 	
 	return true;
 }
+
+/**
+ * This is the ajax callback that will make default content get set up for Theme Bundles.
+ *
+ * @since 1.0
+ * @param void
+ * @return void
+ */
+function mp_stacks_set_up_theme_bundle_default_content_via_ajax(){
+	
+	global $mp_core_options;
+	$mp_core_options = get_option('mp_core_options');
+	
+	$theme_bundle_slug = $_POST['mp_theme_bundle_slug'];
+	
+	//Continue Setting up the default Stacks and corresponsing posts
+	$setup_success = mp_stacks_theme_bundle_create_default_pages( $theme_bundle_slug );
+		
+	if ( $setup_success ){
+		
+		echo json_encode( array(
+			'setup_complete' => true
+		) );
+		
+		die();
+		
+	}
+	else{
+		
+		echo json_encode( array(
+			'setup_complete' => false
+		) );
+		
+		die();
+	}
+		
+		
+}
+add_action( 'wp_ajax_mp_stacks_set_up_theme_bundle_default_content', 'mp_stacks_set_up_theme_bundle_default_content_via_ajax' );
