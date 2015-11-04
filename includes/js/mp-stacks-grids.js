d748f129aa5be5f587cd47339d0602a556ab6468
Phil Johnston <johnstonphilip@gmail.com>
2015-11-04 18:51:31 -0500

    Added "reload from scratch" mode for Isotope Filtering

--- includes/js/mp-stacks-grids.js
+++ includes/js/mp-stacks-grids.js
@@ -16,6 +16,9 @@ jQuery(document).ready(function($){
 			mp_stacks_grid_post_id: mp_stacks_grid_post_id,
 			mp_stacks_grid_offset: $(this).attr( 'mp_stacks_grid_brick_offset' ),
 			mp_stacks_grid_orderby: $(this).attr( 'mp_stacks_grid_orderby' ),
+			
+			mp_stacks_grid_filter_tax: $(this).attr( 'tax' ),
+			mp_stacks_grid_filter_term: $(this).attr( 'term' ),
 		}
 		
 		//Are we using Masonry?
@@ -92,11 +95,14 @@ jQuery(document).ready(function($){
 	var mp_stacks_grid_filter_value = '*';
 	
 	//Initialize Grid Isotopes
-	$( '.mp-stacks-grid-isotope' ).imagesLoaded(function(){
+	$( '.mp-stacks-grid-isotope' ).imagesLoaded( function(){
+		
+		var layout_mode = $(this )[0].elements[0] ? $(this )[0].elements[0].attributes[1].nodeValue : 'masonry';
+		
 		$('.mp-stacks-grid-isotope').isotope({
 		  // main isotope options
 		  itemSelector: '.mp-stacks-grid-item-masonry',
-		  layoutMode: 'masonry',
+		  layoutMode: layout_mode,
 		  gutterWidth: 0,
 		  filter: mp_stacks_grid_filter_value
 		});
@@ -108,15 +114,17 @@ jQuery(document).ready(function($){
 		
 		$( '.mp-stacks-grid-isotope' ).imagesLoaded(function(){
 
+			var layout_mode = $(this )[0].elements[0] ? $(this )[0].elements[0].attributes[1].nodeValue : 'masonry';
+		
 			//Then relayout isotope with the user's currently chosen filter
 			$('.mp-stacks-grid-isotope').isotope({
 			  // main isotope options
 			  itemSelector: '.mp-stacks-grid-item-masonry',
-			  layoutMode: 'masonry',
+			  layoutMode: layout_mode,
 			  gutterWidth: 0,
-			  //filter: mp_stacks_grid_filter_value
-			});	
-				
+			  filter: mp_stacks_grid_filter_value
+			});
+					
 			var wait_for_isotope_filter_to_kick_in = setInterval( function(){ 
 				clearInterval( wait_for_isotope_filter_to_kick_in );
 			
@@ -135,12 +143,15 @@ jQuery(document).ready(function($){
 	$( document ).on( 'mp_stacks_resize_complete', function() {
 		
 		$( '.mp-stacks-grid-isotope' ).imagesLoaded(function(){
+			
+			var layout_mode = $(this )[0].elements[0] ? $(this )[0].elements[0].attributes[1].nodeValue : 'masonry';
+		
 			$('.mp-stacks-grid-isotope').isotope({
 			  // main isotope options
 			  itemSelector: '.mp-stacks-grid-item-masonry',
-			  layoutMode: 'masonry',
+			  layoutMode: layout_mode,
 			  gutterWidth: 0,
-			  //filter: mp_stacks_grid_filter_value
+			  filter: mp_stacks_grid_filter_value
 			});
 				
 			var wait_for_isotope_filter_to_kick_in = setInterval( function(){ 
@@ -157,7 +168,7 @@ jQuery(document).ready(function($){
 	});
 	
 	//Filter grid items when filter button is clicked		
-	$( document ).on( 'click', '.mp-stacks-grid-isotope-sort-container > div', function() {
+	$( document ).on( 'click', '.mp-stacks-grid-isotope-sort-container[mp_stacks_grid_isotope_behavior="default_isotope"] > div', function() {
 		mp_stacks_grid_filter_value = $(this).attr('data-filter');
 		$(this).parent().parent().find( '.mp-stacks-grid-isotope' ).isotope('layout').isotope({ filter: mp_stacks_grid_filter_value } );
 	  
@@ -170,7 +181,7 @@ jQuery(document).ready(function($){
 	});
 	
 	//Filter grid items when filter dropdown is changed
-	$(document).on( 'change', '.mp-stacks-grid-isotope-sort-select', function(){
+	$(document).on( 'change', '.mp-stacks-grid-isotope-sort-container[mp_stacks_grid_isotope_behavior="default_isotope"] .mp-stacks-grid-isotope-sort-select', function(){
 		var mp_stacks_grid_filter_value = $(this).val();
 		$(this).parent().parent().find( '.mp-stacks-grid-isotope' ).isotope('layout').isotope({ filter: mp_stacks_grid_filter_value });
 		
@@ -181,6 +192,125 @@ jQuery(document).ready(function($){
 		}, 500);
 		
 	});
+	
+	/**
+	 * Reload posts from scratch into a grid based on an isotope filter button being clicked.
+	 *
+	 */
+	$( document ).on( 'click', '.mp-stacks-grid-isotope-sort-container[mp_stacks_grid_isotope_behavior="reload_from_scratch"] > div', function( event ){
+		
+		event.preventDefault();
+		
+		$.mp_stacks_isotope_reload_from_scratch( $( this ) );
+	});
+	
+	$( document ).on( 'change', '.mp-stacks-grid-isotope-sort-container[mp_stacks_grid_isotope_behavior="reload_from_scratch"] .mp-stacks-grid-isotope-sort-select', function( event ){
+		
+		event.preventDefault();
+		
+		$.mp_stacks_isotope_reload_from_scratch( $("option:selected", this) );
+		
+	});
+	
+	$.mp_stacks_isotope_reload_from_scratch = function( this_filter_control ) { 
+		
+		var mp_stacks_grid_post_id = this_filter_control.attr( 'mp_stacks_grid_post_id' );
+		
+		// Use ajax to load more posts
+		var postData = {
+			action: 'mp_stacks_' + this_filter_control.attr( 'mp_stacks_grid_ajax_prefix' ) + '_load_more',
+			mp_stacks_grid_post_id: mp_stacks_grid_post_id,
+			mp_stacks_grid_filter_tax: this_filter_control.attr( 'tax' ),
+			mp_stacks_grid_filter_term: this_filter_control.attr( 'term' ),
+			mp_stacks_grid_offset: '0',
+		}
+		
+		//Are we using Masonry?
+		var masonry_on = eval( 'masonry_grid_' + this_filter_control.attr( 'mp_stacks_grid_post_id' ) );
+				
+		var the_grid_container = $( '#mp-brick-' + mp_stacks_grid_post_id + ' .mp-stacks-grid' );
+		var the_button_container = $( '#mp-brick-' + mp_stacks_grid_post_id + ' .mp-stacks-grid-load-more-container' );
+		var the_after_container = $( '#mp-brick-' + mp_stacks_grid_post_id + ' .mp-stacks-grid-after' );
+		
+		//Replace the load more button with an animation to show it is loading more
+		$( '#mp-brick-' + mp_stacks_grid_post_id + ' .mp-stacks-grid-load-more-container > a' ).hide();
+		the_button_container.show();
+		
+		//remove any pre existing elements that we no longer want
+		pre_existing_elements = the_grid_container.children();
+		the_grid_container.isotope( 'remove', pre_existing_elements );
+		
+		//Hide the grid container so the loading animation is tight to the filter buttons.
+		the_grid_container.css( 'display', 'none' );
+		var the_loading_animation = the_button_container.find( '.mp-stacks-grid-load-more-animation-container');
+		
+		the_loading_animation.css( 'margin-top', '20px' ).show();
+		
+		//Ajax load more posts
+		$.ajax({
+			type: "POST",
+			data: postData,
+			dataType:"json",
+			url: mp_stacks_frontend_vars.ajaxurl,
+			success: function (response) {
+				
+				var newitems = $(response.items);
+				newitems.css('visibility', 'hidden' );
+				
+				//Show the grid container again
+				the_grid_container.css( 'display', '' );
+				
+				//Add the new items to the page
+				if ( masonry_on ){
+					
+					//Add new items from ajax request
+					the_grid_container.append( newitems).imagesLoaded( function(){ 
+						
+						//the_grid_container.isotope( 'layout' );
+						the_grid_container.append(newitems).isotope( 'appended', newitems ).isotope('layout');
+									
+						//Add the updated "Load More" button to the page
+						the_button_container.replaceWith(response.button);
+						
+						//Add the animation trigger which resets animations on newly added items
+						the_after_container.html(response.animation_trigger);
+												
+						var wait_for_masonry_to_kick_in = setInterval( function(){ 
+						
+							newitems.css('visibility', 'visible' );
+							
+							the_grid_container.isotope( 'layout' );
+														
+							clearInterval( wait_for_masonry_to_kick_in );
+							//Refresh waypoints to reflect new page size
+							Waypoint.refreshAll();
+						}, 500);
+												
+					}); 
+				}
+				else{
+					
+					the_grid_container.html(newitems);
+					newitems.css('visibility', 'visible' );
+					//Add the updated "Load More" button to the page
+					the_button_container.replaceWith(response.button);
+					//Add the animation trigger which resets animations on newly added items
+					the_after_container.html(response.animation_trigger);
+					
+					var wait_for_masonry_to_kick_in = setInterval( function(){ 
+						clearInterval( wait_for_masonry_to_kick_in );
+						//Refresh waypoints to reflect new page size
+						Waypoint.refreshAll();
+					}, 500);
+						
+				}
+			
+			}
+		}).fail(function (data) {
+			console.log(data);
+		});	
+			
+	}
 	
 	//When the orderby dropdown is changed, refresh the page with the new order set
 	$(document).on( 'change', '.mp-stacks-grid-orderby-select', function(){
